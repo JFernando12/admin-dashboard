0a9b399ca7902e1001caec4702efe5518f0c464e
Fernando Castrejon <fernandocastrejonh@gmail.com>
2023-02-12 10:17:53 -0600

    changes

---
+++ server/src/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import clientRoutes from './routes/clientRoutes.js';
+import generalRoutes from './routes/generalRoutes.js';
+import managementRoutes from './routes/managementRoutes.js';
+import salesRoutes from './routes/salesRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Settings
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('common'));
+app.use(cors());
+app.use(helmet());
+
+// Routes
+app.use('/client', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('/management', managementRoutes);
+app.use('/sales', salesRoutes);
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(
+    'mongodb+srv://jfernando12:contrasena@admin-dashboard.etrbtlv.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then((db) => {
+    console.log('DB Connected: ', db.connection.host);
+    app.listen(PORT, () => {
+      console.log('Server on port: ', PORT);
+    });
+  });
