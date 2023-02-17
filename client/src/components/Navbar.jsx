d31d8926476d4bd2f728cf338b2d6c683e212d10
JFernando12 <fire.jf2012@gmail.com>
2023-02-17 14:39:07 -0600

    add sidebare

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,24 +17,34 @@ import {
   Toolbar,
   IconButton,
   InputBase,
+  MenuItem,
+  Button,
+  Box,
+  Typography,
+  Menu,
 } from '@mui/material';
 
-const Navbar = () => {
+const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
   return (
     <AppBar
       sx={{
         position: 'static',
-        backgroud: 'none',
+        background: 'none',
         boxShadow: 'none',
       }}
     >
       <Toolbar sx={{ justifyContent: 'space-between' }}>
-        {/* LEFT */}
+        {/* LEFT SIDE */}
         <FlexBetween>
-          <IconButton>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
           <FlexBetween
@@ -49,8 +59,9 @@ const Navbar = () => {
             </IconButton>
           </FlexBetween>
         </FlexBetween>
-        {/* RiGHT SIDE */}
-        <FlexBetween>
+
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
           <IconButton onClick={() => dispatch(setMode())}>
             {theme.palette.mode === 'dark' ? (
               <DarkModeOutlined sx={{ fontSize: '25px' }} />
@@ -61,6 +72,40 @@ const Navbar = () => {
           <IconButton>
             <SettingsOutlined sx={{ fontSize: '25px' }} />
           </IconButton>
+
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                textTransform: 'none',
+                gap: '1rem',
+              }}
+            >
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="32px"
+                width="32px"
+                borderRadius="50%"
+                sx={{ objectFit: 'cover' }}
+              />
+              <ArrowDropDownOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: '25px' }}
+              />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            >
+              <MenuItem onClick={handleClose}>Log Out</MenuItem>
+            </Menu>
+          </FlexBetween>
         </FlexBetween>
       </Toolbar>
     </AppBar>
