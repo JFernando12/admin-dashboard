4b890c526b325ba0ed57895269554c43a072bcc4
Fernando Castrejon <fernandocastrejonh@gmail.com>
2023-02-18 12:11:55 -0600

    add overview

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: 'adminApi',
-  tagTypes: ['User', 'Products', 'Customers', 'Transactions'],
+  tagTypes: [
+    'User',
+    'Products',
+    'Customers',
+    'Transactions',
+    'Geography',
+    'Sales',
+  ],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
@@ -25,6 +32,13 @@ export const api = createApi({
       }),
       providesTags: ['Transactions'],
     }),
+    getGeography: build.query({
+      query: () => 'client/geography',
+    }),
+    getSales: build.query({
+      query: () => 'sales/sales',
+      providesTags: ['Sales'],
+    }),
   }),
 });
 
@@ -33,4 +47,6 @@ export const {
   useGetProductsQuery,
   useGetCustomersQuery,
   useGetTransactionsQuery,
+  useGetGeographyQuery,
+  useGetSalesQuery,
 } = api;
