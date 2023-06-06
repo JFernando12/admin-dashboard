542221898783342da13466dbc64a28473fc68f5c
JFernando12 <fire.jf2012@gmail.com>
2023-06-06 10:11:15 -0600

    products finish

--- practice-client/src/scenes/products/index.jsx
+++ practice-client/src/scenes/products/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { Box, Card, CardContent, Typography, useTheme } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Collapse,
+  Rating,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import Header from '../../components/Header';
 import { useGetProductsQuery } from '../../store';
 
 const Product = ({
+  _id,
   name,
   description,
   price,
@@ -13,26 +25,71 @@ const Product = ({
   stat,
 }) => {
   const theme = useTheme();
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <Card
       sx={{
-        backgroundImage: 'none',
         backgroundColor: theme.palette.background.alt,
         borderRadius: '0.55rem',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
       }}
     >
       <CardContent>
-        <Typography>{category}</Typography>
-        <Typography>{name}</Typography>
+        <Typography
+          sx={{ fontSize: 14 }}
+          color={theme.palette.secondary[700]}
+          gutterBottom
+        >
+          {category}
+        </Typography>
+        <Typography variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography sx={{ mb: '1.5rem' }} color={theme.palette.secondary[400]}>
+          {Number(price).toFixed(2)}
+        </Typography>
+        <Rating value={rating} readOnly />
+        <Typography variant="body2">{description}</Typography>
       </CardContent>
+      <CardActions>
+        <Button
+          onClick={() => setIsExpanded(!isExpanded)}
+          variant="primary"
+          size="small"
+        >
+          See More
+        </Button>
+      </CardActions>
+      <Collapse
+        in={isExpanded}
+        timeout="auto"
+        unmountOnExit
+        sx={{
+          color: theme.palette.neutral[300],
+        }}
+      >
+        <CardContent>
+          <Typography>id: {_id}</Typography>
+          <Typography>Supply Left: {supply}</Typography>
+          <Typography>
+            Yearly Sales This Year: {stat.yearlySalesTotal}
+          </Typography>
+          <Typography>
+            Yealy Units Sold This Year: {stat.yearlyTotalSoldUnits}
+          </Typography>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 };
 
 const Products = () => {
-  const { data, isLoading } = useGetProductsQuery();
+  const { data } = useGetProductsQuery();
   console.log(data);
+  const isNonMobile = useMediaQuery('(min-width:1000px)');
 
   return (
     <Box m="1.5rem 2.5rem">
@@ -42,6 +99,11 @@ const Products = () => {
           mt="20px"
           display="grid"
           gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+          rowGap="20px"
+          columnGap="1.33%"
+          sx={{
+            '& > div': { gridColumn: isNonMobile ? undefined : 'span 4' },
+          }}
         >
           {data.map(
             ({
@@ -56,7 +118,7 @@ const Products = () => {
             }) => {
               return (
                 <Product
-                  key={_id}
+                  _id={_id}
                   name={name}
                   description={description}
                   price={price}
